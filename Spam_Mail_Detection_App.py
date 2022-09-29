19628a004f5b04e65cd943c844a82016f6ef4965
AshishParulekar <109098663+AshishParulekar@users.noreply.github.com>
2022-09-29 14:36:24 +0530

    Update Spam_Mail_Detection_App.py

--- Spam_Mail_Detection_App.py
+++ Spam_Mail_Detection_App.py
@@ -64,6 +64,7 @@ def Spam_Detection(text):
 # Streamlit_Code********************
 
 st.title('Spam Mail Detector')
+st.image('https://github.com/AshishParulekar/SPAM_MAIL_DETECTION/blob/main/SPAM.png?raw=true')
 
 C1,C2 = st.columns((8,2))
 C1.write(' ')
